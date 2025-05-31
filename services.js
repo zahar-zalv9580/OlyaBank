0054ed76d2c318ffb7ffee7f20d89e07ec2e4b60
Asiroha <niksiroha@gmail.com>
2025-05-31 10:53:19 +0300

    sdsdffffff

---
+++ services.js
@@ -0,0 +1,7 @@
+let sumacredit = document.querySelector('.suma').value
+let timecredit = document.querySelector('.time').value
+let namecredit = document.querySelector('.name').value
+document.querySelector('.button').addEventListener('click', function(){
+let numercredit = document.querySelector('.numer').value
+let numbernumer = numercredit.split("")
+document.querySelector('p').innerHTML = "4354354"})
